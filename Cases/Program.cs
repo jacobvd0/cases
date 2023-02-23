8bd54c1c44d9321ed0ea7fc78ea215815f751581
Jacob <46667568+jacobvd0@users.noreply.github.com>
2023-02-23 19:23:58 +1030

    Add files via upload

---
+++ Cases/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Cases // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = 3;
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("number is 1");
+                    break;
+                case not 4:
+                    Console.WriteLine("not 4");
+                    break;
+                default:
+                    Console.WriteLine("probably 4");
+                    break;
+            }
+        }
+    }
+}
